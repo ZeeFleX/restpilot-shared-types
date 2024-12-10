131c42f3fd7f53ab87255813147516d8c4749bd2
Ilya Kardapolov <kardapolov@gmail.com>
2024-12-10 12:32:00 +0500

    Removed GQL entity from shared types

--- entities/menu.entities.ts
+++ entities/menu.entities.ts
@@ -1,11 +1,3 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
-
 export interface IMenuCategory {
-  id: number;
-}
-
-@ObjectType()
-export class IMenuCategoryGraphQL implements IMenuCategory {
-  @Field(() => Int, { description: 'Unique ID of menu category' })
-  id: number;
+	id: number;
 }
