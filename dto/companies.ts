786d677aaefe5a0dceea3f01cfd370b7057544e4
Ilya Kardapolov <kardapolov@gmail.com>
2024-12-21 01:14:52 +0500

    CreateCompany type

--- dto/companies.ts
+++ dto/companies.ts
@@ -3,7 +3,7 @@ import { CompaniesEntities } from "src/types/shared";
 export namespace CompaniesDTO {
   export namespace Request {
     export interface CreateCompany
-      extends Omit<CompaniesEntities.Company, "id" | "restaurants"> {}
+      extends Pick<CompaniesEntities.Company, "name" | "address"> {}
   }
   export namespace Response {}
 }
